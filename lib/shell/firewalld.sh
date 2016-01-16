8a371c0c9aa4d05d915c585e001d543d87b6ab72
Vladislav Rastrusny <FractalizeR@yandex.ru>
2016-01-17 01:08:33 +0300

    Added firewall enabling in case it was disabled

--- lib/shell/firewalld.sh
+++ lib/shell/firewalld.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Enabling NFS file sharing for Vagrant
+systemctl enable firewalld.service
+systemctl start firewalld.service
 firewall-cmd --zone=internal --change-interface=vboxnet0
 firewall-cmd --permanent --zone=internal --add-service=nfs
 firewall-cmd --permanent --zone=internal --add-service=rpc-bind
